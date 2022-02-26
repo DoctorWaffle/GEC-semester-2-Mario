f6c8d64d907894af4aa5646253552ae6f235df12
DoctorWaffle <91890510+DoctorWaffle@users.noreply.github.com>
2022-02-26 23:45:55 +0000

    images drawing

--- Mario/MarioGameBase/MarioGameBase/MarioGameBase.cpp
+++ Mario/MarioGameBase/MarioGameBase/MarioGameBase.cpp
@@ -44,8 +44,23 @@ bool InitSDL()
 		{
 			//init PNG loading
 			int imageFlags = IMG_INIT_PNG;
-			if(!(IMG_Init(imageFlags)&imageFlags))
-				//tutorial 4 step 5 here
+			if (!(IMG_Init(imageFlags) & imageFlags))
+			{
+				cout << "SDL_Image could not Initialise. Error: " << IMG_GetError();
+				return false;
+			}
+			//load the background texture
+			g_texture = LoadTextureFromFile("Images/test.bmp");
+			if (g_texture == nullptr)
+			{
+				return false;
+			}
+
+		}
+		else
+		{
+			cout << "Renderer could not Initialise. Error: " << SDL_GetError();
+			return false;
 		}
 		//did window get created?
 		if (g_window == nullptr)
@@ -65,7 +80,12 @@ void CLoseSDL()
 	//quit SDL subsystems
 	IMG_Quit();
 	SDL_Quit();
-
+	
+	//clear the texture
+	FreeTexture();
+	//release the renderer
+	SDL_DestroyRenderer(g_renderer);
+	g_renderer = nullptr;
 }
 bool Update()
 {
@@ -86,6 +106,59 @@ bool Update()
 	return false;
 }
 
+void render()
+{
+	//Clear the screen
+	SDL_SetRenderDrawColor(g_renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+	SDL_RenderClear(g_renderer);
+
+	//set where to render the texture
+	SDL_Rect renderLocation = { 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT };
+
+	//render to screen
+	SDL_RenderCopyEx(g_renderer, g_texture, NULL, &renderLocation, 0, NULL, SDL_FLIP_NONE);
+
+	//update the screen
+	SDL_RenderPresent(g_renderer);
+}
+
+SDL_Texture* LoadTextureFromFile(string path)
+{
+	//remove memory used for a previous texture 
+	FreeTexture();
+
+	SDL_Texture* p_texture = nullptr;
+
+	//Load the image
+	SDL_Surface* p_surface = IMG_Load(path.c_str());
+	if (p_surface != nullptr)
+	{
+		//create the texture from the pixels on the surface
+		p_texture = SDL_CreateTextureFromSurface(g_renderer, p_surface);
+		if (p_texture == nullptr)
+		{
+			cout << "unable to create texture from surface. Error: " << SDL_GetError();
+		}
+		//remove loaded surface with texture
+		SDL_FreeSurface(p_surface);
+		
+	}
+	else
+	{
+		cout << "unable to create texture from surface. Error: " << IMG_GetError();
+	}
+	return p_texture;
+}
+
+void FreeTexture()
+{
+	//check if texture exists before removing it
+	if (g_texture != nullptr)
+	{
+		SDL_DestroyTexture(g_texture);
+		g_texture = nullptr;
+	}
+}
 int main(int argc, char* args[])
 {
 	if (InitSDL())
@@ -96,6 +169,7 @@ int main(int argc, char* args[])
 		//game loop
 		while (!quit)
 		{
+			render();
 			quit = Update();
 		}
 	}
